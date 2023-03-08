c1074b615fc15a95d2c0ee72cc92563b728f3918
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2023-03-07 19:38:31 -0800

    Add doctest support

---
+++ test/doctest_test.cpp
@@ -0,0 +1,11 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <doctest.h>
+
+SCENARIO("TETS") {
+    GIVEN("TEST") {
+        WHEN("TEST") {
+            CHECK(1 + 1 == 2);
+        }
+    }
+}
