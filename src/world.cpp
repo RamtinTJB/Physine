b627ac6bd8b162e58eb37742e5578c220e4f35ce
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2023-03-15 16:26:06 -0700

    Add rudimentary collisions detection

--- src/world.cpp
+++ src/world.cpp
@@ -1,12 +1,15 @@
 #include "world.h"
+#include <iostream>
 
 World::World() {
     clock.restart();
+    graphics = new Graphics(1500, 1500);
 }
 
 void World::update_velocities(double dt) {
     for (Object* obj : objects_) {
-        obj->velocity += (1/obj->mass)*g*dt;
+        if (gravity_)
+            obj->velocity += g*dt;
     }
 }
 
@@ -16,14 +19,40 @@ void World::update_transforms(double dt) {
     }
 }
 
+void World::check_collisions() {
+    for (Object* obj1 : objects_) {
+        for (Object* obj2 : objects_) {
+            if (obj1 == obj2) continue;
+            if (obj1->collider != nullptr && obj2->collider != nullptr) {
+                if (obj1->collider->test_collision(dynamic_cast<CircleCollider*>(obj2->collider))) {
+                    // TODO Resolve Collision
+                    obj1->velocity = Vector2f{-obj1->velocity[0], -obj1->velocity[1]};
+                }
+            }
+        }
+    }
+}
+
+void World::draw_objects() {
+    for (Object* obj: objects_) {
+        if (obj->is_drawable()) {
+            obj->drawable->render(obj->transform);
+        }
+    }
+}
+
 void World::mainloop() {
     while (true) {
+        if (graphics->has_closed()) return;
         double dt = clock.delta_time();
+        clock.restart();
 
         update_velocities(dt);
         update_transforms(dt);
+        check_collisions();
 
-        clock.restart();
+        draw_objects();
+        graphics->draw_objects(objects_);
     }
 }
 
--- src/world.h
+++ src/world.h
@@ -6,21 +6,32 @@
 #include "object.h"
 #include "common.h"
 #include "clock.h"
+#include "graphics.h"
 
-const Vector2f g {0, -9.81};
+const Vector2f g {0, 200};
 
 class World {
     private:
         std::vector<Object*> objects_;
         Clock clock;
+        Graphics* graphics = nullptr;
+
+        bool gravity_ = true;
 
         void update_velocities(double dt);
         void update_transforms(double dt);
+
+        void check_collisions();
+
+        void draw_objects();
     
     public:
         World();
+
         void add_object(Object* obj) { objects_.push_back(obj); }
         void mainloop();
+
+        void set_gravity(bool gravity) { gravity_ = gravity; }
 
         ~World();
 };
