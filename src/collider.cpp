36ad97ba3792b0aae5fb0949330de9242051fd25
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2023-03-15 16:25:21 -0700

    Refactor Collider

--- src/collider.cpp
+++ src/collider.cpp
@@ -1,18 +1,21 @@
 #include "collider.h"
+#include <iostream>
 
-bool BoxCollider::test_collision(const BoxCollider& bc) const {
-    return box.intersects(bc.box);
+bool BoxCollider::test_collision(const BoxCollider* bc) const {
+    return box.intersects(bc->box);
 }
 
-bool BoxCollider::test_collision(const CircleCollider& cc) const {
+bool BoxCollider::test_collision(const CircleCollider* cc) const {
     // TODO Implement circle v box collision
     return false;
 }
 
-bool CircleCollider::test_collision(const BoxCollider& bc) const {
-    return bc.test_collision(*this);
+bool CircleCollider::test_collision(const BoxCollider* bc) const {
+    return bc->test_collision(this);
 }
 
-bool CircleCollider::test_collision(const CircleCollider& cc) const {
-    return circle.intersects(cc.circle);
+bool CircleCollider::test_collision(const CircleCollider* cc) const {
+    Circle<double> this_circle(transform->position, transform->scale.x()); 
+    Circle<double> other_circle(cc->get_transform()->position, cc->get_transform()->scale.x());
+    return this_circle.intersects(other_circle);
 }
--- src/collider.h
+++ src/collider.h
@@ -3,33 +3,42 @@
 
 #include "rect.h"
 #include "circle.h"
+#include "transform.h"
 
 class BoxCollider;
 class CircleCollider;
 
 class Collider {
     public:
-        virtual bool test_collision(const BoxCollider&) const = 0;
-        virtual bool test_collision(const CircleCollider&) const = 0;
+        Collider(Transform* t) : transform{t} {}
+        virtual bool test_collision(const Collider*) const = 0;
+        virtual bool test_collision(const BoxCollider*) const = 0;
+        virtual bool test_collision(const CircleCollider*) const = 0;
         virtual ~Collider() = default;
+
+        Transform* get_transform() const { return transform; }
+
+        Transform* transform;
 };
 
 class BoxCollider : public Collider {
     public:
         Rect<double> box;
 
-        bool test_collision(const BoxCollider&) const;
-        bool test_collision(const CircleCollider&) const;
+        bool test_collision(const Collider*) const { return false; }
+        bool test_collision(const BoxCollider*) const;
+        bool test_collision(const CircleCollider*) const;
 
         ~BoxCollider() {}
 };
 
 class CircleCollider : public Collider {
     public:
-        Circle<double> circle;
+        using Collider::Collider;
 
-        bool test_collision(const BoxCollider&) const;
-        bool test_collision(const CircleCollider&) const;
+        bool test_collision(const Collider*) const { return false; }
+        bool test_collision(const BoxCollider*) const;
+        bool test_collision(const CircleCollider*) const;
 
         ~CircleCollider() {}
 };
