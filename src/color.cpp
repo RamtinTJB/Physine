cbf869be37418ee4e1a2366865ef6b797aa2cd8d
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2023-03-22 17:32:04 -0700

    Add HSL and HEX support

--- src/color.cpp
+++ src/color.cpp
@@ -1,4 +1,61 @@
+#include <cmath>
+#include <vector>
+#include <random>
+#include <cstdio>
+
 #include "color.h"
+#include "common.h"
+
+namespace {
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist255(0, 255);
+
+    double hue_to_rgb(double p, double q, double t) {
+        if (t < 0) t += 1;
+        if (t > 1) t -= 1;
+        if (t < 1.0/6.0) return p + (q - p) * 6 * t;
+        if (t < 1.0/2.0) return q;
+        if (t < 2.0/3.0) return p + (q - p) * (2.0/3.0 - t) * 6;
+        return p;
+    }
+}
+
+Color::Color(const std::initializer_list<int>& list) {
+    std::vector<int> v(list);
+    r = v[0];
+    g = v[1];
+    b = v[2];
+    a = 255;
+}
+
+Color Color::random_color() {
+    int r = dist255(rng);
+    int g = dist255(rng);
+    int b = dist255(rng);
+    return Color{r, g, b};
+}
+
+Color Color::FromHSL(double h, double s, double l) {
+    double r, g, b;
+    if (std::abs(s) < EPSILON) {
+        r = l; g = l; b = l;
+    } else {
+        double q = l < 0.5 ? l * (1.0 + s) : l + s - l*s;
+        double p = 2*l - q;
+        r = hue_to_rgb(p, q, h+1.0/3.0);
+        g = hue_to_rgb(p, q, h);
+        b = hue_to_rgb(p, q, h-1.0/3.0);
+    }
+
+    return Color{int(std::round(r*255)), int(std::round(g*255)), int(std::round(b*255)) };
+} 
+
+Color Color::FromHEX(const std::string& hex) {
+    int r, g, b;
+    sscanf(hex.c_str(), "#%02x%02x%02x", &r, &g, &b);
+    return Color{r, g, b};
+}
 
 const Color Color::WHITE       {255, 255, 255};
 const Color Color::SILVER      {192, 192, 192};
--- src/color.h
+++ src/color.h
@@ -2,15 +2,8 @@
 #define PHYSINE_COLOR_H_
 
 #include <initializer_list>
-#include <vector>
-#include <random>
 #include <SFML/Graphics.hpp>
-
-namespace {
-    std::random_device dev;
-    std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> dist255(0, 255);
-}
+#include <string>
 
 class Color {
     public:
@@ -19,24 +12,16 @@ class Color {
         Color() : r{0}, g{0}, b{0}, a{255} {}
         Color(int r, int g, int b) : r{r}, g{g}, b{b}, a{255} {}
         Color(int r, int g, int b, int a) : r{r}, g{g}, b{b}, a{a} {}
-        Color(const std::initializer_list<int>& list) {
-            std::vector<int> v(list);
-            r = v[0];
-            g = v[1];
-            b = v[2];
-            a = 255;
-        }
+        Color(const std::initializer_list<int>&);
 
-        static Color random_color() {
-            int r = dist255(rng);
-            int g = dist255(rng);
-            int b = dist255(rng);
-            return Color{r, g, b};
-        }
+        static Color random_color();
         
         operator sf::Color () const {
             return sf::Color(r, g, b, a);
         }
+
+        static Color FromHSL(double h, double s, double l);
+        static Color FromHEX(const std::string&);
 
         static const Color WHITE;
         static const Color SILVER;
