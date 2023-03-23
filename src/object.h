4968f8ab75e49a06027b0b17daed80432f5273f2
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2023-03-23 11:08:44 -0700

    Change Objecti into class and add custom properties

--- src/object.h
+++ src/object.h
@@ -2,40 +2,54 @@
 #define PHYSINE_OBJECT_H_
 
 #include <string>
+#include <unordered_map>
 
 #include "collider.h"
 #include "common.h"
 #include "drawable.h"
 #include "transform.h"
+#include "utils.h"
 
-struct Object {
-    std::string name;
+class Object {
+    public:
+        const std::string name;
 
-    double mass;
-    bool has_gravity = true;
-    bool is_kinetic = true;
-    Vector2f velocity;
+        double mass;
+        bool has_gravity = true;
+        bool is_kinetic = true;
+        Vector2f velocity;
+        
+        Transform* transform = nullptr;
+        Collider* collider = nullptr;
+        Drawable* drawable = nullptr;
+
+        Object() = delete;
+        Object(const std::string& name) : name{name}, mass{0.} {
+            transform = new Transform();
+        }
+
+        void add_collider(Collider* c) { collider = c; }
+        void add_property(const std::string& key, const std::string& value) {
+            custom_properties_[key] = value; 
+        }
+
+        std::string get_property(const std::string& key) {
+            if (map_contains(custom_properties_, key)) return custom_properties_[key];
+            return "";
+        }
+
+        bool is_drawable() const { return drawable != nullptr; }
+
+        ~Object() {
+            delete transform;
+            if (collider != nullptr)
+                delete collider;
+            if (drawable != nullptr)
+                delete drawable;
+        }
     
-    Transform* transform = nullptr;
-    Collider* collider = nullptr;
-    Drawable* drawable = nullptr;
-
-    Object() = delete;
-    Object(const std::string& name) : name{name}, mass{0.} {
-        transform = new Transform();
-    }
-
-    void add_collider(Collider* c) { collider = c; }
-
-    bool is_drawable() const { return drawable != nullptr; }
-
-    ~Object() {
-        delete transform;
-        if (collider != nullptr)
-            delete collider;
-        if (drawable != nullptr)
-            delete drawable;
-    }
+    private:
+        std::unordered_map<std::string, std::string> custom_properties_;
 };
 
 #endif
