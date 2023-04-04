ae8ed6416650a21557bbf285f4b1aad19025db7c
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2023-04-04 13:55:25 -0700

    Implement rotation around a point

--- src/ph_math.cpp
+++ src/ph_math.cpp
@@ -2,15 +2,21 @@
 #include "constants.h"
 
 double degree_to_radian(double degree) {
-    return degree*PI/180;
+    return degree*PI/180.0;
 }
 
 double radian_to_degree(double radian) {
-    return radian*180/PI;
+    return radian*180.0/PI;
 }
 
 Vector2f rotate_rad(const Vector2f& input, const Vector2f& origin, double angle_rad) {
-    return Vector2f();
+    Vector2f position_wrt_origin = input - origin;
+    angle_rad *= -1;
+    double new_x = std::cos(angle_rad)*position_wrt_origin.x() -
+        std::sin(angle_rad)*position_wrt_origin.y() + origin.x();
+    double new_y = std::sin(angle_rad)*position_wrt_origin.x() +
+        std::cos(angle_rad)*position_wrt_origin.y() + origin.y();
+    return Vector2f{new_x, new_y};
 }
 
 Vector2f rotate(const Vector2f& input, const Vector2f& origin, double angle_deg) {
--- test/math_test.cpp
+++ test/math_test.cpp
@@ -8,3 +8,17 @@ SCENARIO("Approx Test") {
     CHECK(1.0 == Approx(1.000001));
     CHECK(100.0 == Approx(95.0).epsilon_ratio(0.06));
 }
+
+SCENARIO("Rotation Test") {
+    Vector2f output = rotate(Vector2f{3, 4}, Vector2f(), 30);
+    CHECK(output.x() == Approx(4.598076));
+    CHECK(output.y() == Approx(1.964101));
+
+    output = rotate(Vector2f{3, 4}, Vector2f() , -30);
+    CHECK(output.x() == Approx(0.5980762));
+    CHECK(output.y() == Approx(4.9641016));
+
+    output = rotate(Vector2f{4, 8}, Vector2f{2, 5}, 47);
+    CHECK(output.x() == Approx(5.558057825));
+    CHECK(output.y() == Approx(5.583287677));
+}
